f7b05c4def8b3677762048c0775763565eb9ace1
mathieu-tatat <91736481+mathieu-tatat@users.noreply.github.com>
2022-01-26 07:40:36 +0100

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'antoine-maherault_wordpress' );
+define( 'DB_NAME', 'mathieu-tatat_wordpress' );
 
 /** MySQL database username */
-define( 'DB_USER', 'wordpress' );
+define( 'DB_USER', 'wordpress1' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', 'wordpress' );
+define( 'DB_PASSWORD', 'wordpress1' );
 
 /** MySQL hostname */
 define( 'DB_HOST', 'localhost:3306' );
